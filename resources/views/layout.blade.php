5c4fa4921c25bca858aa4ed93b8b04298b26f363
falko Sam-Sin <86917@glr.nl>
2023-01-30 15:27:42 +0100

    niks

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- SEO -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Damn! It's a chair">
+    <title>Chair | Get a Chair</title>
+
+    <!-- icon -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="/favicon.ico" type="image/x-icon">
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    
+</head>
+<body class=" bg-white" >
+    @include('components.navbar')
+    @yield('Falko_branch')
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+@section('main')
+{{-- <div class="container"> --}}
+<div class="home_dex bg-white" >
+    <header class="bg-primary mt-4 ps-4 pb-3 pt-3"> <a class="text-dark " href="/">WholeSale</a>
+       
+    </header>
+    {{-- mt-4 
+    py-2  mt-4 
+    py-2   mt-3  --}}
+  
+     
+</div>
+
+@include('components.chaircontainer')
+   
+{{-- </div> --}}
+@endsection
